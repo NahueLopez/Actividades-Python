a11f99428cc740fc0c6bc98e2c884cde541ab9b2
Nahuel Luis López <78488563+NahueLopez@users.noreply.github.com>
2024-07-02 22:16:35 -0300

    Delete Dia 6 directory

--- Dia 6/LimpiarConsola.py
+++
@@ -1,8 +0,0 @@
-from os import system #Esto es para importar lo que necesitamos para limpiar la consola
-#Ademas tenemos que ir a run - debug - editar configuracion - execution - tildad emulator
-nombre = input("Dime tu nombre: ")
-edad = input("Dime tu edad: ")
-
-system("cls")
-
-print(f"Tu nombre es {nombre} y tienes {edad} años")
